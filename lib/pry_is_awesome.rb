89ce8d19fa5e962bfadb4e46ad10d32651acb4b0
ERIC KELLY <eakelly220@gmail.com>
2018-04-30 22:11:26 +0000

    automatically backed up by learn

--- lib/pry_is_awesome.rb
+++ lib/pry_is_awesome.rb
@@ -1,5 +1,6 @@
 require 'pry'
 
+<<<<<<< HEAD
 def prying_into_the_method
 	inside_the_method = "We're inside the method"
 	puts inside_the_method
@@ -8,3 +9,13 @@ def prying_into_the_method
 	puts this_variable_hasnt_been_interpreted_yet
 end
 
+=======
+
+prying_into_the_method
+
+def plus_two(num)
+binding.pry
+return num + 2 
+  
+end 
+>>>>>>> fad7ffc23b3b722beae8c3480541480bcfc762b6
